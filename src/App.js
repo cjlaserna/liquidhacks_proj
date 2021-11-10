6cf15b0784058222410bd55a86afb181be40af6a
redxzeta <xzetanzero@gmail.com>
2021-11-09 20:37:39 -0500

    FOrum

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavigationBar from "./layout/NavigationBar";
-import Forum from "./pages/forum/Forum";
+import Job from "./pages/forum/Job";
+
+import MainForum from "./pages/forum/MainForum";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import NoMatch from "./pages/NoMatch/NoMatch";
 import Register from "./pages/register/Register";
 import { supabase } from "./supabase/supabaseClient";
 
@@ -24,7 +27,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/forum" element={<Forum />} />
+        <Route path="/forum">
+          <Route index element={<MainForum />} />
+          <Route path="job" element={<Job />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/forum/Job.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+import Forum from "./Forum";
+
+const Job = () => {
+  return (
+    <Forum>
+      <Container>
+        <h1>Esports Job and Industry Discussion</h1>
+      </Container>
+    </Forum>
+  );
+};
+
+export default Job;
