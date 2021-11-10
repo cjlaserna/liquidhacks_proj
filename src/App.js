396b03f487dfef1c8cf405651c14fe6bce62731c
cjlaserna <49669196+cjlaserna@users.noreply.github.com>
2021-11-09 22:33:45 -0800

    completed landing page, added pictures

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      <footer></footer>
     </BrowserRouter>
   );
 }
